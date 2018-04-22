392fc313bf11cd2685e17b9bb73732f0988e9f43
Marina Yazykova <4152@htl.rennweg.at>
2018-04-22 21:03:23 +0200

    Änderung mit HTML vorgenommen

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
 <body>
 
 <script>
@@ -18,7 +27,27 @@
 
 </script>
 
-<button onclick="doAjax()" value="click"></button>
+<div class="jumbotron text-center">
+    <h1>Choose your ball</h1>
+    <p class="text-primary">To change material change the URL </p>
+    <p>(This website is for professional programmers.)</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <h3>Column <mark>HTML</mark></h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+
+        </div>
+        <div class="col-sm-4">
+            <h3>Column <mark>AJAX</mark></h3>
+            <button onclick="doAjax()" value="click">Load Ajax</button>
+        </div>
+    </div>
+</div>
+
+
 
 <?php
 /**
